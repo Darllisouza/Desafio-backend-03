e8dc618388c768f81ef202f0fe0fc0408d02bc4f
Larissa081208 <larissaevelinregoazevedo@gmail.com>
2023-09-19 16:39:11 -0300

    rota "login" criada

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -10,7 +10,7 @@ const cadastrarUsuario = async (req, res) => {
 		const emailExiste = await pool.query('select * from usuarios where email = $1',[email])
 	
 		if (emailExiste.rowCount > 0) {
-			return res.status(400).json({ mensagem: 'Email já existe!' })
+			return res.status(400).json({ mensagem: 'Já existe usuário cadastrado com o e-mail informado!' })
 		}
 	
 		const senhaCriptografada = await bcrypt.hash(senha, 10)
@@ -31,9 +31,41 @@ const cadastrarUsuario = async (req, res) => {
 	
 }
 
+const login = async (req, res) => {
+	const { email, senha } = req.body
+	
+	try {
+		const { rows, rowCount } = await pool.query(
+			'select * from usuarios where email = $1',
+			[email]
+		)
+	
+		if (rowCount === 0) {
+			return res.status(400).json({ mensagem: 'Email ou senha inválida' })
+		}
+	
+		const { senha: senhaUsuario, ...usuario } = rows[0]
+	
+		const senhaCorreta = await bcrypt.compare(senha, senhaUsuario)
+	
+		if (!senhaCorreta) {
+			return res.status(400).json({ mensagem: 'Email ou senha inválida' })
+		}
+	
+		const token = jwt.sign({ id: usuario.id }, senhaJwt, { expiresIn: '1h' })
+	
+		return res.json({
+			usuario,
+			token,
+		})
+	} catch (error) {
+		return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+    }
+}
 
 
 module.exports =
 {
- cadastrarUsuario
+ cadastrarUsuario,
+ login
 }
--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { cadastrarUsuario } = require("./controladores/usuarios");
+const { cadastrarUsuario, login } = require("./controladores/usuarios");
 const rotas = express();
 
 rotas.post("/usuario", cadastrarUsuario)
+rotas.post("/login", login)
 module.exports = rotas
