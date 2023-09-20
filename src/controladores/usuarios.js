0a873a81ef31d553e3fded4e4af872ac2d46ccef
Larissa081208 <larissaevelinregoazevedo@gmail.com>
2023-09-20 16:26:17 -0300

    retirei rota login do arquivo de usuarios

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -32,43 +32,8 @@ const cadastrarUsuario = async (req, res) => {
 
 }
 
-//rever aula 14/09 e corrigir o token
-const login = async (req, res) => {
-	const { email, senha } = req.body
-	
-	try {
-		const { rows, rowCount } = await pool.query(
-			'select * from usuarios where email = $1', //testar no beekeeper antes
-			[email]
-		)
-	
-		if (rowCount === 0) {
-			return res.status(400).json({ mensagem: 'Email ou senha inválida' }) //Não dizer onde está o erro deixa o código mais seguro
-		}
-	
-		const { senha: senhaUsuario, ...usuario } = rows[0]
-	
-		const senhaCorreta = await bcrypt.compare(senha, senhaUsuario) //verificar a senha válida
 
-	    //compara a senha que ta no banco com a senha do usuarios
-		if (!senhaCorreta) {
-			return res.status(400).json({ mensagem: 'Email ou senha incorreta' })
-		}
-	
-		const token = jwt.sign({ id: usuario.id }, senhaJwt, { expiresIn: '1h' })
-	
-		return res.json({
-			usuario,
-			token,
-
-		})
-		
-	} catch (error) {
-		return res.status(500).json({ mensagem: 'Erro interno do servidor' })
-    }
-}
-
-
+/*
 const detalharUsuario = async (req, res) => {
 	try {
 		const queryBanco = 'select * from usuarios where email = $1';
@@ -85,13 +50,11 @@ const detalharUsuario = async (req, res) => {
 		return res.status(400).json({ mensagem: 'Token inválido!'})
 	}
 }
-
+*/
 
 
 module.exports =
 {
- cadastrarUsuario,
- login,
- detalharUsuario
+ cadastrarUsuario
  
 }
